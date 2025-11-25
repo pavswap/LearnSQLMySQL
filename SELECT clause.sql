c42d8e3a3ef50fb12d4543a3245753ffb260cd4b
Paavan Swaroop <paavanswaroop2@gmail.com>
2025-11-25 19:13:35 +0530

    SELECt Clause for Selecting colums from rows

---
+++ SELECT clause.sql
@@ -0,0 +1,12 @@
+SELECT * FROM list;
+
+SELECT name, age FROM list;
+
+SELECT * FROM list
+    WHERE name != 'Paavan';
+
+SELECT * FROM list
+    WHERE name IN ('Subash', 'Ram', 'Hammy');
+
+SELECT * FROM list
+    WHERE age BETWEEN 18 AND 60;
